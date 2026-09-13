beded1a3e2b1ea02f33f56f92e6372369f99d58a
Nicolás Romani <nicolasromani.dev@gmail.com>
2026-09-13 11:35:17 -0300

    update eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,45 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import importSort from 'eslint-plugin-simple-import-sort';
+import eslintPluginTailwindcss from 'eslint-plugin-tailwindcss';
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+   ...nextVitals,
+   ...nextTs,
+   globalIgnores([
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
+   ]),
+   eslintPluginTailwindcss.configs.recommended,
+   {
+      plugins: {
+         'simple-import-sort': importSort
+      },
+      settings: {
+         tailwindcss: {
+            cssConfigPath: './src/app/globals.css'
+         }
+      },
+      rules: {
+         'prefer-const': 'off',
+         '@typescript-eslint/no-unused-vars': [
+            'warn',
+            {
+               'varsIgnorePattern': '^_',
+               'argsIgnorePattern': '^_',
+               'caughtErrorsIgnorePattern': '^_'
+            }
+         ],
+         'react-hooks/exhaustive-deps': 'off',
+         'react-hooks/purity': 'off',
+         'quotes': ['error', 'single'],
+         'simple-import-sort/imports': 'error',
+         'simple-import-sort/exports': 'error'
+      }
+   }
 ]);
 
 export default eslintConfig;
